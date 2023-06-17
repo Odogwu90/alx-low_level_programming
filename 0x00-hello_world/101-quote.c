eba06fbf013239c1c6b80a56c53b01f97feb60ca
Odogwu90 <okparamartins14@gmail.com>
2023-06-17 07:36:40 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main(void)
-{
+int main(void){
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 
