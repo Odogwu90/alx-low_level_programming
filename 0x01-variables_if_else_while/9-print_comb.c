53b0ae68cc96e408bb6708491336d5dec8a5a067
Odogwu90 <okparamartins14@gmail.com>
2023-06-19 15:36:09 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,15 +6,16 @@
 */
 int main(void)
 {
-int n;
+int num;
 
-for (n = 48; n <= 57; n++)
+for (num = 0; num <= 9; num++)
 {
-putchar(n);
+putchar((num % 10) + '0');
+if (num == 9)
+continue;
 putchar(',');
 putchar(' ');
 }
 putchar('\n');
-}
 return (0);
 }
