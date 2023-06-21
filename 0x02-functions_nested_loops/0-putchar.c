907754ba3f315ea3d28f682109b76c9950916af4
Odogwu90 <okparamartins14@gmail.com>
2023-06-21 11:25:58 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
 *Main: Entry point
-*Description: Print_putcher to stdio
+*Description: This program demonstrates the usage of the putchar function to print a character
 *Return: always o
 */
 int main(void)
 {
+char ch = 'A';
 _putchar('_');
 _putchar('p');
 _putchar('u');
