038380ff38bb1627f276d5169c067ec7c6a64c8b
Odogwu90 <okparamartins14@gmail.com>
2023-06-21 10:56:11 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*Main: Entry point
+*Description: Print_putcher to stdio
+*Return: always o
+*/
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
